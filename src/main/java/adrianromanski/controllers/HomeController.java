7cb7a56330918880584639909e8196b09b5d9700
Adrian Romański <muszynianka1337@gmail.com>
2020-03-14 14:08:18 +0100

    Added Total Number of Reported Cases

--- src/main/java/adrianromanski/controllers/HomeController.java
+++ src/main/java/adrianromanski/controllers/HomeController.java
@@ -1,10 +1,13 @@
 package adrianromanski.controllers;
 
+import adrianromanski.models.LocationStats;
 import adrianromanski.services.CoronaVirusDataService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class HomeController {
@@ -14,7 +17,10 @@ public class HomeController {
 
     @GetMapping("/")
     public String home(Model model) {
-        model.addAttribute("locationsStats", coronaVirusDataService.getAllStats());
+        List<LocationStats> allStats = coronaVirusDataService.getAllStats();
+        int totalCases = allStats.stream().mapToInt(LocationStats::getLatestTotalCases).sum();
+        model.addAttribute("locationsStats", allStats);
+        model.addAttribute("totalReportedCases", totalCases);
         return "home";
     }
 }
